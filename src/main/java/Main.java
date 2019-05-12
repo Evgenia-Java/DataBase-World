7bf53f279f33ca643562b21206c29c504aaa4018
evgenia <evgeniashmachina@mail.ru>
2019-05-12 23:05:38 +0300

    DataBase - World

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,25 @@
+import entity.City;
+import org.hibernate.Session;
+
+import javax.swing.*;
+
+
 public class Main {
     public static void main(String[] args) {
 
-
+        Session session = null;
+        try{
+            session = SessionFactoryUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            session.save(City);
+            session.getTransaction().commit();
+        } catch (Exception e){
+            JOptionPane.showMessageDialog(null,e.getMessage(), "Error insert", JOptionPane.OK_CANCEL_OPTION);
+        } finally {
+            if (session != null && session.isOpen()){
+                session.close();
+            }
+        }
 
 
     }
