9afbabddd1704e28d48ea84225b0408895828c1a
Evgenia Shmachina <evgeniashmachina@mail.ru>
2019-05-13 11:57:03 +0300

    DataBase - World

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,21 +7,23 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
 
-        City city = new City();
-        Session session = null;
-        try{
-            session = SessionFactoryUtil.getSessionFactory().openSession();
-            session.beginTransaction();
-            session.save(city);
-            session.getTransaction().commit();
-        } catch (Exception e){
-            JOptionPane.showMessageDialog(null,e.getMessage(), "Error insert", JOptionPane.OK_CANCEL_OPTION);
-        } finally {
-            if (session != null && session.isOpen()){
-                session.close();
-            }
-        }
+//        City city = new City();
+//        Session session = null;
+//        try{
+//            session = SessionFactoryUtil.getSessionFactory().openSession();
+//            session.beginTransaction();
+//            session.save(city);
+//            session.getTransaction().commit();
+//        } catch (Exception e){
+//            JOptionPane.showMessageDialog(null,e.getMessage(), "Error insert", JOptionPane.OK_CANCEL_OPTION);
+//        } finally {
+//            if (session != null && session.isOpen()){
+//                session.close();
+//            }
+//        }
 
+        int id_city = 48;
+        SessionFactoryUtil.getSessionFactory().openSession().get(City.class, id_city);
 
     }
 }
